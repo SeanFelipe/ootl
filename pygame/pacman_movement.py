8b6e710a9e167ab8eab6acfa6334396d0977a62e
Sean Wolfe <sean.wolfe@csaa.com>
2017-02-02 11:19:55 -0800

    spritesheet, movement, more

--- pygame/pacman_movement.py
+++ pygame/pacman_movement.py
@@ -1,23 +1,38 @@
 import pygame
 import sys
 
-screen = pygame.display.set_mode((640,480))
-# returns a Surface
-spritesheet = pygame.image.load('spritesheet.png')
-screen.blit(spritesheet, (0,0))
+screen = pygame.display.set_mode((640,480)) # returns a Surface
+spritesheet = pygame.image.load('spritesheet.png') # also returns a surface
+screen.blit(spritesheet, (0,0)) # draws spritesheet onto the screen
+# the above ^^ will NOT display until you call pygame.display.update() !
+
 # get the pacman image
 # study this method to find the argument sequence
 # https://www.pygame.org/docs/ref/surface.html#pygame.Surface.subsurface
 # then add the correct arguments to Surface.subsurface() to get a pacman image
 # pacman = spritesheet.subsurface()
-# spritesheet.blit(pacman, (300, 50))
+# screen.blit(pacman, (300, 50))
 
-u = pygame.display.update
-u()
+startx = 100
+starty = 100
+pacmanx, pacmany = startx, starty
+
+
 
 running = True
 while running:
+    screen.blit(pacman, pacmanx, pacmany)
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
             pygame.quit()
+        # what is the event type for a keyboard press?
+        # https://www.pygame.org/docs/ref/event.html
+        elif e.type == pygame.?????:
+            # how do we get which key was pressed?
+            # what keys do we care about?
+            # what direction to we need to adjust based on which key is pressed?
+            # adjust pacmanx
+            # adjust pacmany
+    pygame.display.update()
+
---
+++ pygame/pacman_spritesheet.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+
+screen = pygame.display.set_mode((640,480))
+# returns a Surface
+spritesheet = pygame.image.load('spritesheet.png')
+screen.blit(spritesheet, (0,0))
+# get the pacman image
+# study this method to find the argument sequence
+# https://www.pygame.org/docs/ref/surface.html#pygame.Surface.subsurface
+# then add the correct arguments to Surface.subsurface() to get a pacman image
+# pacman = spritesheet.subsurface()
+# spritesheet.blit(pacman, (300, 50))
+
+u = pygame.display.update
+u()
+
+running = True
+while running:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            running = False
+            pygame.quit()
