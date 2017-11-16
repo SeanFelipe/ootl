20d5703c3295aea472763e09d1fd19b80adb266f
Sean Wolfe <sean@ootl.net>
2017-11-16 08:10:39 -0800

    fixup wall_only

--- pygame/pmmore/collide/wall_only.py
+++ pygame/pmmore/collide/wall_only.py
@@ -4,18 +4,17 @@ from pygame.transform import scale2x
 screen = pygame.display.set_mode((300,300))
 spritesheet = pygame.image.load('spritesheet.png')
 
-wallx = 680 / 3 * 2 - 4
-wall_width = 5
-wall_height = 100
-wall = scale2x(spritesheet.subsurface(wallx, 2, wall_width, wall_height))
-pacman = scale2x(spritesheet.subsurface(680 / 3 * 2, 0, 16, 16))
+wall = scale2x(spritesheet.subsurface(448, 2, 5, 100))
+pacman = scale2x(spritesheet.subsurface(453, 0, 16, 16))
 
+wallx, wally = 200, 70
+pacmanx, pacmany = 50, 120
 
 running = True
 while running:
     pygame.display.update()
-    screen.blit(wall, (200, 70))
-    screen.blit(pacman, (50,120))
+    screen.blit(wall, (wallx, wally))
+    screen.blit(pacman, (pacmanx, pacmany))
 
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
