5ce554e6921f83adbcb9d4103f78e8087aa488ad
Sean Wolfe <sean@ootl.net>
2018-06-10 20:21:08 -0700

    mario java, from a while back but never committed

---
+++ java/mario/desktop/src/srg/ootl/mario/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package srg.ootl.mario.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import srg.ootl.mario.OotlMario;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OotlMario(), config);
+	}
+}
